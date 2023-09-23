f7ef701629e2d566e81776c7dbef81ac34d5e4d1
DanielChachagua <danielmchachagua@gmail.com>
2023-09-23 12:14:49 -0300

    ADD: visualizacion de mensajes

--- js/servidor/cargarMensajes.js
+++ js/servidor/cargarMensajes.js
@@ -27,21 +27,35 @@ function obtenerMensajesDelCanal(id_canal) {
         const seccionDerecha = document.querySelector(".servidor-search");
         seccionDerecha.innerHTML = "";
         const divTituloCanal = document.createElement("div");
-        divTituloCanal.className = 'titulo-canal';
+        divTituloCanal.className = 'canal-seleccionado';
         const seccionMensajes = document.createElement("div");
         seccionMensajes.className = 'seccion-mensajes';
-        
+        const divInputMensaje = document.createElement("div");
+        divInputMensaje.className = "input-mensaje";
+        //divTituloCanal.style.height = "8vh";
 
         
         data[0].forEach(function(mensaje) {
-            console.log(mensaje)
-            const pMensaje = document.createElement("p");
-            pMensaje.textContent = mensaje.mensaje;
-            pMensaje.className = "p-mensajes";
-            seccionMensajes.appendChild(pMensaje);
+            const divImagenMensaje = document.createElement("div");
+            const divUsernameFecha = document.createElement("div");
+            const divMensaje = document.createElement("div");
+
+            divImagenMensaje.className = "div-imagen-mensaje";
+            divUsernameFecha.className = "div-username-fecha";
+            divMensaje.textContent = mensaje.mensaje;
+            divMensaje.className = "div-mensajes";
+
+
+            const idUsuario = document.querySelector(".username-usuario");
+            if (mensaje.usuario.id === parseInt(idUsuario.id)){
+                divMensaje.style.alignSelf = "end";
+                divMensaje.style.backgroundColor = "#010931";   
+            }
+            seccionMensajes.appendChild(divMensaje);
         });
         seccionDerecha.appendChild(divTituloCanal);
         seccionDerecha.appendChild(seccionMensajes);
+        seccionDerecha.appendChild(divInputMensaje);
         return data;
     })
     .catch(error => {
